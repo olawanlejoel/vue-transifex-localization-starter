6476c58cbde034405927bc05972e3f15bdde52e7
Olawanle Joel <info4tobest2@gmail.com>
2022-01-31 12:24:49 +0000

    first push

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
-# vue-localization
+# Code for vue-localization demo
+
+> You need to create a [Transifex account](https://www.transifex.com/signup/), create a project, and obtain native credentials. You should copy the secret and token for later usage.
 
 ## Project setup
 ```
@@ -19,6 +21,7 @@ yarn build
 ```
 yarn lint
 ```
+> Also, in package.json replace <TOKEN> and <SECRET> in the scripts push-translation and refresh-translation with your API token and secret.
 
 ### Customize configuration
 See [Configuration Reference](https://cli.vuejs.org/config/).
--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,99 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+	<div class="main">
+		<div class="profile-header">
+			<div class="profile-img">
+				<img src="./assets/user.jpg" class="profile-pic" alt="" />
+			</div>
+			<h2>Joel Olawanle</h2>
+			<span>Web developer"</span>
+		</div>
+		<div class="profile-info">
+			<p>Joel Olawanle is a Frontend developer with expertise in vue.js and React.js alongside Jamstack frameworks like Nuxt, 
+					Next.js and Gridsome. He is highly skilled at converting images/UI designs into frontend codes (UI developer). He is also a 
+					Technical writer interested in making the web accessible to everyone by always looking for ways to give back to the tech community. 
+					Furthermore, he is currently taking a deep dive into React and Headless CMS of which he is doing a lot of writing on as he learns 
+					new things. He is a big fan of open source."
+			</p>
+		</div>
+	</div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+	export default {
+		name: 'App',
+	};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+	* {
+		margin: 0;
+		padding: 0;
+		box-sizing: border-box;
+	}
+
+	body {
+		font-family: Arial, Helvetica, sans-serif;
+		min-height: 100vh;
+		width: 100vw;
+		background: rgb(9, 6, 32);
+		color: #fff;
+		margin: 0 auto;
+	}
+
+	.main {
+		margin: 0 25%;
+		padding: 100px;
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		gap: 20px;
+	}
+
+	@media (max-width: 500px) {
+		.main {
+			margin: 0 10%;
+			padding: 30px 10px;
+		}
+	}
+
+	.profile-header {
+		display: flex;
+		align-items: center;
+		flex-direction: column;
+	}
+
+	img {
+		max-width: 100%;
+	}
+
+	.profile-pic {
+		width: 250px;
+		border-radius: 50%;
+		object-fit: cover;
+		border: 5px solid #fff;
+		margin-bottom: 15px;
+	}
+
+	h2 {
+		font-size: 35px;
+	}
+
+	p {
+		margin-top: 20px;
+		font-size: 23px;
+		text-align: center;
+		color: rgb(223, 221, 221);
+	}
+
+	@media (max-width: 500px) {
+		p {
+			margin-top: 20px;
+			font-size: 18px;
+			text-align: center;
+		}
+	}
+
+	.lang{
+		padding: 10px;
+	}
 </style>
